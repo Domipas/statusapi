8bd17b0be27a3b0ce9b108ab5b1dfae69286add5
Raoul de Heer <raouldeheer@users.noreply.github.com>
2021-01-31 20:20:36 +0100

    🩹Fixed #86
    Fixed #86 crash on dns fail.
    Added dns A-record check.

--- ts/tests/HttpTest.ts
+++ ts/tests/HttpTest.ts
@@ -1,6 +1,7 @@
 import http, { IncomingMessage } from 'http';
 import Test from './Test';
 import { lanchano } from "../middleware/Middleware";
+import { promises as dns } from 'dns';
 
 export default class HttpTest extends Test {
 
@@ -12,6 +13,7 @@ export default class HttpTest extends Test {
 
     public async checkTest(): Promise<void> {
         try {
+            await dns.resolve4(this.adres);
             http.request(this.adres, (res: IncomingMessage) => {
                 this.status = res.statusCode ?? 0;
                 this.timeChecked = new Date();
